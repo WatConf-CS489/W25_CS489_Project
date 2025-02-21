4128d9746644269b1ec5eae262dcdf19a2cb633c
Kabir Oberai <oberai.kabir@gmail.com>
2025-02-21 15:59:22 -0500

    Use nginx

--- backend/src/extensions.py
+++ backend/src/extensions.py
@@ -1,11 +1,8 @@
 import logging
-from flask_cors import CORS
 from src.base import db
 from src.base import app
 
 logging.getLogger().setLevel(logging.INFO)
-
-CORS(app, supports_credentials=True)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://postgres:password@db:5432/postgres"
 db.init_app(app)
