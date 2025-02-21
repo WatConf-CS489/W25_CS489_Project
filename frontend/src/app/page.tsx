b0e9ecef252d74da710fba7f4793d807a5e9ee95
Kabir Oberai <oberai.kabir@gmail.com>
2025-02-21 03:35:17 -0500

    Set up frontend-backend communication

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,22 @@
+"use client";
+
+import { QueryClient, QueryClientProvider, useQuery } from "@tanstack/react-query";
+
+const queryClient = new QueryClient()
+
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+  return <QueryClientProvider client={queryClient}>
+    <Content />
+  </QueryClientProvider>;
+}
+
+function Content() {
+  const response = useQuery({
+    queryKey: ["hello"],
+    queryFn: async () => {
+      const response = await fetch("http://localhost:8000/");
+      return response.text();
+    }
+  })
+  return <h1>{response.data || "Loading..."}</h1>;
 }
