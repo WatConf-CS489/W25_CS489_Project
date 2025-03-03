8111d49e22c6cd0092f3f3699518db30dc14a5e0
boyazhang314 <boyazhangboi@gmail.com>
2025-03-03 12:39:21 -0500

    ui tweaks

--- frontend/app/user/dashboard/page.tsx
+++ frontend/app/user/dashboard/page.tsx
@@ -9,21 +9,27 @@ import moment from "moment";
 import ContentWrapper from "@/components/ContentWrapper";
 import PageHeader from "@/components/PageHeader";
 
-import { Alert, IconButton, List, ListItem, Snackbar, Typography } from "@mui/material";
+import {
+  Alert,
+  CircularProgress,
+  IconButton,
+  List,
+  ListItem,
+  Snackbar,
+  Typography,
+} from "@mui/material";
 import { Box, styled } from "@mui/system";
 import PersonIcon from "@mui/icons-material/Person";
 
 const postSchema = yup.object({
   time: yup.number().required(),
-  liked: yup.boolean().required(),
-  likes: yup.number().required(),
+  liked: yup.boolean().optional(),
+  likes: yup.number().optional(),
   content: yup.string().required(),
-})
+});
 
 const responseSchema = yup.object({
-  posts: yup
-    .array(postSchema)
-    .required(),
+  posts: yup.array(postSchema).required(),
 });
 
 type PostType = yup.InferType<typeof postSchema>;
@@ -58,7 +64,10 @@ const Post = ({ post }: { post: PostType }) => {
   });
 
   return (
-    <ListItem alignItems="center" sx={{ display: "flex", flexDirection: "column", paddingBottom: "40px" }}>
+    <ListItem
+      alignItems="center"
+      sx={{ display: "flex", flexDirection: "column", paddingBottom: "40px" }}
+    >
       <PostMetadata>
         <ProfileImage>
           <PersonIcon sx={{ color: "#000000", scale: 1.5 }} />
@@ -84,20 +93,23 @@ export default function Page() {
       time: 1740992164,
       liked: false,
       likes: 1017,
-      content: "This course provides an introduction to building secure software applications. It examines the software development life cycle and teaches what developers can do in each step to make their software more secure.  It also will cover common vulnerabilities that exist and how developers can avoid or safeguard against them. Students completing this course should be able to build and deploy software with fewer security issues. Intended audience: Fourth year CS students (CS 45X)"
+      content:
+        "This course provides an introduction to building secure software applications. It examines the software development life cycle and teaches what developers can do in each step to make their software more secure.  It also will cover common vulnerabilities that exist and how developers can avoid or safeguard against them. Students completing this course should be able to build and deploy software with fewer security issues. Intended audience: Fourth year CS students (CS 45X)",
     },
     {
       time: 1740819364,
       liked: false,
       likes: 5796,
-      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
     },
     {
       time: 1740974164,
       liked: false,
       likes: 0,
-      content: "A button is a fastener that joins two pieces of fabric together by slipping through a loop or by sliding through a buttonhole. In modern clothing and fashion design, buttons are commonly made of plastic but also may be made of metal, wood, or seashell. Buttons can also be used on containers such as wallets and bags."
-    }
+      content:
+        "A button is a fastener that joins two pieces of fabric together by slipping through a loop or by sliding through a buttonhole. In modern clothing and fashion design, buttons are commonly made of plastic but also may be made of metal, wood, or seashell. Buttons can also be used on containers such as wallets and bags.",
+    },
   ];
 
   const fetchPosts = async () => {
@@ -107,12 +119,13 @@ export default function Page() {
         throw new Error();
       }
       return await responseSchema.validate(await response.json());
-    } catch {
+    } catch (e) {
+      console.error(e);
       setError(true);
       return { posts: temp_fallback_posts };
     }
-  }
-  
+  };
+
   const { data } = useQuery({
     queryKey: ["user"],
     queryFn: fetchPosts,
@@ -120,22 +133,25 @@ export default function Page() {
 
   return (
     <>
-      <Box sx={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
+      <Box
+        sx={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}
+      >
         <PageHeader hasPostButton={true} />
         <ContentWrapper>
-          <List sx={{ overflowY: "auto", paddingTop: "3vh" }}>
-            {data && data.posts ?
-              data.posts.map((post, index) => (
-                <Post key={index} post={post} />
-              )) :
-              <Typography variant="body1">
-                Loading posts...
-              </Typography>}
+          <List sx={{ overflowY: "auto", paddingTop: "3vh", width: "100%" }}>
+            {data && data.posts ? (
+              data.posts.map((post, index) => <Post key={index} post={post} />)
+            ) : (
+              <Box sx={{ display: "flex", justifyContent: "center" }}>
+                <CircularProgress />
+              </Box>
+            )}
           </List>
           {data && data.posts && (
             <Typography variant="body1" marginBottom="40px">
               ~~That&#39;s all for now~~
-            </Typography>)}
+            </Typography>
+          )}
           <Snackbar
             open={error}
             anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
