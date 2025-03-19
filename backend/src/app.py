8bba19de33559df506a18bd7765ae7f22aeee35e
DannyZhang686 <65862897+DannyZhang686@users.noreply.github.com>
2025-03-18 20:13:57 -0400

    ahahahahahahaa

--- backend/src/app.py
+++ backend/src/app.py
@@ -4,6 +4,6 @@ import src.routes
 import src.models
 
 @app.after_request
-def add_csp_header(resp)
+def add_csp_header(resp):
     resp.headers["Content-Security-Policy"] = "default-src 'self'; frame-ancestors 'self';"
     return resp
