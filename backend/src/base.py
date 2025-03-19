5eb945f7154fbf9d3f3f8aa35c81fe8e9dd3eebe
DannyZhang686 <65862897+DannyZhang686@users.noreply.github.com>
2025-03-18 22:25:44 -0400

    :sob: idk lol

--- backend/src/base.py
+++ backend/src/base.py
@@ -3,10 +3,6 @@ from sqlalchemy.orm import DeclarativeBase
 
 app = Flask(__name__)
 
-@app.after_request
-def add_csp_header(resp):
-    resp.headers["Content-Security-Policy"] = "default-src 'self'; frame-ancestors 'self';"
-    return resp
 
 class DBModel(DeclarativeBase):
     pass
