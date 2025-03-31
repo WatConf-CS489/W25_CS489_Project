e9471dbeacba735b450d8108dec70f65f5fc80b6
tjandhya <jtanmayi@gmail.com>
2025-03-30 20:06:09 -0400

    fix dashboard_routes

--- backend/src/dashboard/dashboard_routes.py
+++ backend/src/dashboard/dashboard_routes.py
@@ -20,7 +20,7 @@ def read_all_posts():
         all_posts = db.session.execute(
             select(Post)
             .join(Post.user)
-            .where(User.deleted_at.is_(None), Post.is_deleted.is_(False))
+            .where(User.deleted_at.is_(None), Post.deleted_at.is_(None))
             .order_by(desc(Post.created_at))
         ).scalars().all()
 
